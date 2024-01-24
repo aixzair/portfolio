2486cefaeb33d63bb87c2eed12d7d723597bc605
Alexandre Lerosier <alex.lerosier@gmail.com>
2024-01-24 17:19:48 +0100

    feat : page IW

--- js/header.js
+++ js/header.js
@@ -9,21 +9,21 @@ fabriquerheader();
 function fabriquerheader() {
     const nav = document.createElement("nav");
     const nom = document.createElement("span");
-    const accueil = document.createElement("a");
+    const projets = document.createElement("a");
     const profil = document.createElement("a");
     const contact = document.createElement("a");
 
     nom.textContent = "Alexandre Lerosier";
-    accueil.textContent = "Accueil";
-    accueil.setAttribute("href", "/index.html")
     profil.textContent = "Profil";
-    profil.setAttribute("href", "/pages/profil.html");
+    profil.setAttribute("href", "/index.html");
+    projets.textContent = "Projets";
+    projets.setAttribute("href", "/pages/projets.html")
     contact.textContent = "Contact";
     contact.setAttribute("href", "/pages/contact.html");
 
     switch (HEADER.dataset.actif) {
-    case "Accueil":
-        accueil.classList.add("actif");
+    case "Projets":
+        projets.classList.add("actif");
         break
     case "Profil":
         profil.classList.add("actif");
@@ -34,8 +34,8 @@ function fabriquerheader() {
     }
 
     nav.appendChild(nom);
-    nav.appendChild(accueil);
     nav.appendChild(profil);
+    nav.appendChild(projets);
     nav.appendChild(contact);
 
     HEADER.appendChild(nav);
